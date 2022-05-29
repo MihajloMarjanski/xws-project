0dedf384a5260f330174f027fe7ef84a02016e96
GGmaz <kajtes.nikola@gmail.com>
2022-05-29 15:08:55 +0200

    initial setup for xws front

---
+++ front/dislinkt/src/app/app.module.ts
@@ -0,0 +1,102 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import {MatTableModule} from '@angular/material/table';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule  } from '@angular/material/icon';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule} from '@angular/material/card';
+import { MatBadgeModule} from '@angular/material/badge';
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatStepperModule} from '@angular/material/stepper';
+import { MatRadioModule} from '@angular/material/radio';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatListModule} from '@angular/material/list';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatCarouselModule } from 'ng-mat-carousel';
+import {MatSortModule} from '@angular/material/sort';
+import {Sort} from '@angular/material/sort';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { InterceptorService } from './service/interceptor.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { RegistrationPageComponent } from './component/registration-page/registration-page.component';
+import { LoginPageComponent } from './component/login-page/login-page.component';
+import { UserProfileComponent } from './component/user-profile/user-profile.component';
+
+const MaterialComponents = [
+  MatSliderModule,
+  MatCarouselModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatGridListModule,
+  MatCardModule,
+  MatBadgeModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatStepperModule,
+  MatRadioModule,
+  MatDividerModule,
+  MatListModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegistrationPageComponent,
+    LoginPageComponent,
+    UserProfileComponent,
+  ],
+  imports: [
+    MaterialComponents,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialComponents,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  providers: [{ provide: JWT_OPTIONS, useValue: JWT_OPTIONS }, JwtHelperService,
+    { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front/dislinkt/src/app/service/user.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Credentials } from '../model/credentials';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+/* import { Client } from '../model/client';
+import { CompanyOwner } from '../model/company-owner';
+import { Company } from '../model/company'; */
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  
+  
+  private _baseUrl = 'https://localhost:8000/';
+  private _login = this._baseUrl + 'auth/login';
+  private _getAllUsernames = this._baseUrl + 'auth/getAllUsernames';
+  private _getCompanies = this._baseUrl + 'company/all';
+  private _blackList = this._baseUrl + 'auth/password/blackList/';
+  private _submitRegistrationOwner  = this._baseUrl + 'company/owner/create';
+  private _submitRegistrationClient  = this._baseUrl + 'clients/create';
+  private _clientByUsername  = this._baseUrl + 'clients/username/';
+  private _adminByUsername  = this._baseUrl + 'admin/username/';
+  private _ownerByUsername  = this._baseUrl + 'company/owner/username/';
+  private _approveCompany  = this._baseUrl + 'admin/approve/company/';
+  private _editOwner  = this._baseUrl + 'company/owner/update';
+  private _editClient  = this._baseUrl + 'clients/update';
+  private _editAdmin  = this._baseUrl + 'admin/update';
+  private _forgotPassword  = this._baseUrl + 'clients/newPassword/';
+  private _companyByOwnerUsername  = this._baseUrl + 'company/';
+  private _apiKey  = this._baseUrl + 'company/owner/apiKey/';
+  
+  constructor(private _http: HttpClient) { }
+  
+
+  
+
+  forgotPassword(username: string) : Observable<any> {
+    return this._http.put<any>(this._forgotPassword + username, {})
+                  .pipe(tap(data =>  console.log('All: ' + JSON.stringify(data))),
+                  catchError(this.handleError)); 
+  }
+
+  logIn(credentials: Credentials): Observable<any> {
+    const body=JSON.stringify(credentials);
+    console.log(body)
+    return this._http.post(this._login, body,{responseType: 'text'})
+  }
+
+  getAllUsernames(): Observable<string[]> { 
+    return this._http.get<string[]>(this._getAllUsernames)
+                          .pipe(tap(data =>  console.log('All: ' + JSON.stringify(data))),
+                            catchError(this.handleError)); 
+  }
+
+  checkBlackListPass(pass: string): Observable<any> { 
+    return this._http.get<any>(this._blackList + pass, {responseType: 'text' as 'json'})
+                          .pipe(tap(data =>  console.log('All: ' + JSON.stringify(data))),
+                            catchError(this.handleError)); 
+  }
+
+  /* createClient(client: Client) : Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+   });
+    const body=JSON.stringify(client);
+    console.log(body)
+    return this._http.post(this._submitRegistrationClient, body, {'headers':headers})
+  }
+
+  approveCompany(id: number) : Observable<any> {
+    return this._http.put<any>(this._approveCompany + id, {})
+                  .pipe(tap(data =>  console.log('All: ' + JSON.stringify(data))),
+                  catchError(this.handleError)); 
+  }
+
+  editAdmin(owner: Client) : Observable<any> {
+    const body=JSON.stringify(owner);
+    console.log(body)
+    return this._http.post(this._editAdmin, body)
+  } */
+  
+
+
+  private handleError(err : HttpErrorResponse) {
+    console.log(err.message);
+    return Observable.throw(err.message);
+    throw new Error('Method not implemented.');
+  } 
+}
