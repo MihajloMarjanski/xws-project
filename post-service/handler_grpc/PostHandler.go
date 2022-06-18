a8c785567e556f590af1ce2e345b95cfc290f1e5
boskoo <andrija.ancobosnjakovic@gmail.com>
2022-06-18 04:46:32 +0200

    implement logs for post-service

--- post-service/handler_grpc/PostHandler.go
+++ post-service/handler_grpc/PostHandler.go
@@ -3,6 +3,8 @@ package handler_grpc
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
 	"post-service/model"
 	"post-service/service"
 	"strconv"
@@ -10,12 +12,29 @@ import (
 
 	pb "github.com/MihajloMarjanski/xws-project/common/proto/post_service"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/natefinch/lumberjack"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc/metadata"
 )
 
 type PostHandler struct {
 	pb.UnimplementedPostServiceServer
 	postService *service.PostService
+}
+
+func init() {
+
+	f := &lumberjack.Logger{
+		Filename:   "./logs/testlogrus.log",
+		MaxSize:    10, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28,   //days
+		Compress:   true, // disabled by default
+	}
+
+	mw := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(mw)
+	log.SetLevel(log.InfoLevel)
 }
 
 func Verify(accessToken string) (*model.Claims, error) {
@@ -25,22 +44,27 @@ func Verify(accessToken string) (*model.Claims, error) {
 		func(token *jwt.Token) (interface{}, error) {
 			_, ok := token.Method.(*jwt.SigningMethodHMAC)
 			if !ok {
+				log.WithFields(log.Fields{"service_name": "post-service", "method_name": "Verify"}).Warn("Unexpected token signing method.")
 				return nil, fmt.Errorf("unexpected token signing method")
 			}
 
+			log.WithFields(log.Fields{"service_name": "post-service", "method_name": "Verify"}).Info("Token successfully verified.")
 			return []byte("tajni_kljuc_za_jwt_hash"), nil
 		},
 	)
 
 	if err != nil {
+		log.WithFields(log.Fields{"service_name": "post-service", "method_name": "Verify"}).Warn("Invalid token.")
 		return nil, fmt.Errorf("invalid token: %w", err)
 	}
 
 	claims, ok := token.Claims.(*model.Claims)
 	if !ok {
+		log.WithFields(log.Fields{"service_name": "post-service", "method_name": "Verify"}).Warn("Invalid token claims.")
 		return nil, fmt.Errorf("invalid token claims")
 	}
 
+	log.WithFields(log.Fields{"service_name": "post-service", "method_name": "Verify"}).Info("Token successfully verified.")
 	return claims, nil
 }
 
@@ -59,9 +83,11 @@ func New() (*PostHandler, error) {
 
 	postService, err := service.New()
 	if err != nil {
+		log.WithFields(log.Fields{"service_name": "post-service", "method_name": "NewPostHandler"}).Error("Error creating post service.")
 		return nil, err
 	}
 
+	log.WithFields(log.Fields{"service_name": "post-service", "method_name": "NewPostHandler"}).Info("Successfully created post handler.")
 	return &PostHandler{
 		postService: postService,
 	}, nil
@@ -74,6 +100,7 @@ func (handler *PostHandler) CreatePost(ctx context.Context, request *pb.CreatePo
 	response := &pb.CreatePostResponse{
 		Id: createdPost.ID.Hex(),
 	}
+	log.WithFields(log.Fields{"service_name": "post-service", "method_name": "CreatePost", "user_id": GetUserID(ctx)}).Info("Successfully created post.")
 	return response, nil
 }
 
@@ -84,6 +111,7 @@ func (handler *PostHandler) AddComment(ctx context.Context, request *pb.AddComme
 	response := &pb.AddCommnetResponse{
 		Id: comment.Text,
 	}
+	log.WithFields(log.Fields{"service_name": "post-service", "method_name": "AddComment", "user_id": GetUserID(ctx), "post_id": comment.PostID}).Info("Successfully added comment.")
 	return response, nil
 }
 
@@ -94,6 +122,7 @@ func (handler *PostHandler) AddLike(ctx context.Context, request *pb.AddLikeRequ
 	response := &pb.AddLikeResponse{
 		Id: strconv.FormatUint(uint64(like.UserID), 10),
 	}
+	log.WithFields(log.Fields{"service_name": "post-service", "method_name": "AddLike", "user_id": GetUserID(ctx), "post_id": like.PostID}).Info("Successfully added like.")
 	return response, nil
 }
 
@@ -104,6 +133,7 @@ func (handler *PostHandler) AddDislike(ctx context.Context, request *pb.AddDisli
 	response := &pb.AddLikeResponse{
 		Id: strconv.FormatUint(uint64(like.UserID), 10),
 	}
+	log.WithFields(log.Fields{"service_name": "post-service", "method_name": "AddDislike", "user_id": GetUserID(ctx), "post_id": like.PostID}).Info("Successfully added dislike.")
 	return response, nil
 }
 
@@ -112,5 +142,6 @@ func (handler *PostHandler) GetPostsForUser(ctx context.Context, request *pb.Use
 	posts := handler.postService.GetPostsForUser(uint(id))
 	pbPosts := mapPostsToProto(posts)
 	response := &pb.PostsResponse{Post: pbPosts}
+	log.WithFields(log.Fields{"service_name": "post-service", "method_name": "CreatePost", "user_id": GetUserID(ctx)}).Info("Successfully retireved posts for user.")
 	return response, nil
 }
