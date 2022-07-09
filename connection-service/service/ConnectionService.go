b8730eab1046845ccf6bec9605c8b6a655e299e5
MihajloMarjanski <marjanski.mihajlo@gmail.com>
2022-07-09 21:01:40 +0200

    added grpc to connection service

--- connection-service/service/ConnectionService.go
+++ connection-service/service/ConnectionService.go
@@ -1,1 +1,13 @@
 package service
+
+type ConnectionService struct {
+}
+
+func New() (*ConnectionService, error) {
+
+	return &ConnectionService{}, nil
+}
+
+func (connectionService *ConnectionService) Connect(id1, id2 int64) {
+
+}
