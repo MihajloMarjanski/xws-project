841643425da4446ae09d500458141ce0ce35296b
MihajloMarjanski <marjanski.mihajlo@gmail.com>
2022-07-09 21:43:14 +0200

    small addition

--- connection-service/service/ConnectionService.go
+++ connection-service/service/ConnectionService.go
@@ -1,5 +1,9 @@
 package service
 
+import (
+	"fmt"
+)
+
 type ConnectionService struct {
 }
 
@@ -9,5 +13,6 @@ func New() (*ConnectionService, error) {
 }
 
 func (connectionService *ConnectionService) Connect(id1, id2 int64) {
-
+	t := fmt.Sprintf("CONNECTIG ID1: %[1]d and ID2: %[2]d\n", id1, id2)
+	fmt.Println(t)
 }
