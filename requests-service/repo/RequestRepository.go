3916e8de39810e2411be92ca1718fb853d31e446
MihajloMarjanski <marjanski.mihajlo@gmail.com>
2022-07-10 22:08:35 +0200

    some fixes

--- requests-service/repo/RequestRepository.go
+++ requests-service/repo/RequestRepository.go
@@ -57,6 +57,13 @@ func (repo *RequestsRepository) GetAllByRecieverId(rid uint) []model.Request {
 }
 
 func (repo *RequestsRepository) AcceptRequest(sid, rid uint) {
+	crtTlsPath, _ := filepath.Abs("./service.pem")
+
+	creds, err6 := credentials.NewClientTLSFromFile(crtTlsPath, "")
+	if err6 != nil {
+		//log.Fatalf("could not process the credentials: %v", err6)
+	}
+
 	request := model.Request{
 		SenderID:   sid,
 		ReceiverID: rid,
@@ -69,6 +76,17 @@ func (repo *RequestsRepository) AcceptRequest(sid, rid uint) {
 	}
 
 	repo.db.Create(&connection)
+	conn, err := grpc.Dial("connection-service:8700", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	client := pbConnection.NewConnectionServiceClient(conn)
+	response, err := client.Connect(context.Background(), &pbConnection.UsersConnectionRequest{&pbConnection.UserPair{Id1: uint64(rid), Id2: uint64(sid)}})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(response)
 }
 
 func (repo *RequestsRepository) DeclineRequest(sid, rid uint) {
